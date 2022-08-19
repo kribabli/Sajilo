ca8d5194f7a3bb229186e1a748a51dbb360b109c
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-08-19 15:20:04 +0530

    ShopNow.java..

---
+++ app/src/main/java/com/sample/sajilo/ShopNow.java
@@ -0,0 +1,14 @@
+package com.sample.sajilo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShopNow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_now);
+    }
+}
